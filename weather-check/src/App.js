43d8e09e7b2aca1003455677b4dd5ba6e5130608
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2023-06-20 22:16:00 +0900

    Add files via upload

---
+++ weather-check/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const API_KEY = 'e017a366fa7438634b996223f898a108'; // Replace with your own OpenWeatherMap API key
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.log('Error fetching weather data:', error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchWeatherData();
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather Check</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>{weatherData.weather[0].description}</p>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
